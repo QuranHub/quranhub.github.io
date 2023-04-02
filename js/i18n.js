9509735f193cd4ef254d1641263acda26c0237fc
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-04-02 20:07:35 +0200

    Finish adding Arabic localization

--- js/i18n.js
+++ js/i18n.js
@@ -16,7 +16,18 @@ var content = {
             understand_quran_description: "Read Quran tafsir (explanation) & translation in various languages.",
             useful_feature: "Useful feature",
             useful_feature_title: "Search in the Quran",
-            useful_feature_description: "Search with a keyword across the entire Quran or just in a Juz', Hizb, Quarter or single Sura"
+            useful_feature_description: "Search with a keyword across the entire Quran or just in a Juz', Hizb, Quarter or single Sura",
+            highlight_verse_title: "Highlight a verse to show more options",
+            bookmarks_feature_title: "Bookmarks & notes",
+            bookmarks_feature_description: "Bookmark a verse or add a text & audio note to it to help you in studying & memorizing the Quran",
+            share_feature_title: "Share",
+            share_feature_description: "Share a verse text via other apps",
+            screenshots_section_title: "Take a closer look",
+            screenshots_section_description: "QuranHub has many features that you'll find useful",
+            download_now: "Download it now",
+            created_with_love: "Created with ❤️ in Cairo, Egypt",
+            support_link: "Support us ❤️",
+            contact_us: "Contact us",
         
         }
     },
@@ -37,7 +48,18 @@ var content = {
             understand_quran_description: "اقرأ تفسير القرآن الكريم أو ترجمته بالعديد من اللغات.",
             useful_feature: "ميزة مفيدة",
             useful_feature_title: "البحث في القرآن",
-            useful_feature_description: "ابحث عن كلمة معينة في القرآن الكريم كله أو فقط في جزء منه أو حزب أو ربع أو سورة"
+            useful_feature_description: "ابحث عن كلمة معينة في القرآن الكريم كله أو فقط في جزء منه أو حزب أو ربع أو سورة",
+            highlight_verse_title: "اضغط على الآية لعرض المزيد من الخيارات",
+            bookmarks_feature_title: "الإشارات المرجعية و الملاحظات",
+            bookmarks_feature_description: "أضف إشارة مرجعية للآية أو أضف ملاحظة نصية أو صوتية لها لمساعدتك في دراسة و حفظ القرآن الكريم",
+            share_feature_title: "المشاركة",
+            share_feature_description: "شارك نص الآية مع الآخرين عبر تطبيقات أخرى",
+            screenshots_section_title: "خذ نظرة أقرب",
+            screenshots_section_description: "يحتوي تطبيق القرآن الكريم على العديد من الميزات التي ستجدها مفيدة",
+            download_now: "حمّله الآن",
+            created_with_love: "تم إنشاؤه  بـ ❤️ في القاهرة، مصر",
+            support_link: "❤️ ادعمنا",
+            contact_us: "تواصل معنا",
 
         }
     }
@@ -112,6 +134,17 @@ function updateContent() {
     setLocalizedText('#useful-feature-label', 'useful_feature')
     setLocalizedText('#useful-feature-title', 'useful_feature_title')
     setLocalizedText('#useful-feature-description', 'useful_feature_description')
+    setLocalizedText('#highlight-verse-feature-title', 'highlight_verse_title')
+    setLocalizedText('#bookmarks-feature-title', 'bookmarks_feature_title')
+    setLocalizedText('#bookmarks-feature-description', 'bookmarks_feature_description')
+    setLocalizedText('#share-feature-title', 'share_feature_title')
+    setLocalizedText('#share-feature-description', 'share_feature_description')
+    setLocalizedText('#screenshots-section-title', 'screenshots_section_title')
+    setLocalizedText('#screenshots-section-description', 'screenshots_section_description')
+    setLocalizedText('#download-now-label', 'download_now')
+    setLocalizedText('#created-with-love-label', 'created_with_love')
+    setLocalizedText('#support-link', 'support_link')
+    setLocalizedText('#contact-label', 'contact_us')
 
 }
 
